7325984f874e910f4b97ae13b7c3917ac8f0c57b
laurabacon <laura_kessinger@yahoo.com>
2024-02-08 19:47:31 -0700

    portfolio page

--- LB-Portfolio/src/pages/Contact.jsx
+++ LB-Portfolio/src/pages/Contact.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from '../components/Header'; 
+import Footer from '../components/Footer';
 
 const Contact = () => {
   return (
     <section id="contact">
       <div>
-        <h3>Contact</h3>
+      <Header />
+      <h3>Contact</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem necessitatibus facilis sunt, ullam quaerat pariatur a sequi nihil alias cumque dolorum possimus dolor exercitationem labore. Nihil debitis saepe a atque?</p>
+      <Footer />
       </div>
     </section>
   );
--- LB-Portfolio/src/pages/Resume.jsx
+++ LB-Portfolio/src/pages/Resume.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from '../components/Header'; 
+import Footer from '../components/Footer';
 
 const Resume = () => {
   return (
     <section id="resume">
       <div>
-        <h3>Resume</h3>
+      <Header />
+      <h3>Resume</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem necessitatibus facilis sunt, ullam quaerat pariatur a sequi nihil alias cumque dolorum possimus dolor exercitationem labore. Nihil debitis saepe a atque?</p>
+      <Footer />
       </div>
     </section>
   );
